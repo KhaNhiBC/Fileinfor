a283138107d6d354cac455971c83b5a724db4cfb
KhaNhi <KhaNhi@users.noreply.github.com>
2024-04-25 19:26:33 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string sourceFilePath = @"D:\baitap\Source.txt"; //File nguồn
+        string desFilePath = @"D:\baitap\Des.txt"; //File đích
+        File.Copy(sourceFilePath,desFilePath, true);
+    }
+
+}
